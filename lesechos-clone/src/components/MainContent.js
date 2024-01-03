3540ba1c8cd26bec6781e59b5abe1c2c8172011d
itou944 <64033053+itou944@users.noreply.github.com>
2024-01-03 12:37:10 +0100

    Add files via upload

---
+++ lesechos-clone/src/components/MainContent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './MainContent.css';
+
+const MainContent = () => {
+    return (
+        <main>
+            <article>
+                <h2>Article Title</h2>
+                <p>Some content about the article...</p>
+            </article>
+            <article>
+                <h2>Another Article Title</h2>
+                <p>Some content about the article...</p>
+            </article>
+        </main>
+    );
+};
+
+export default MainContent;
---
+++ lesechos-clone/src/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>ரொலி news</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Business</li>
+                    <li>Politics</li>
+                    <li>Tech</li>
+                    <li>Opinions</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
